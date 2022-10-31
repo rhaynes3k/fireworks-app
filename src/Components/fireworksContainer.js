8d1fc81cc222123fb15f7ba66c4eab14d5665b9e
rhaynes3k <rhaynes3k@gmail.com>
2022-10-30 22:52:28 -0500

    Completed search feature.

--- src/Components/fireworksContainer.js
+++ src/Components/fireworksContainer.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import FireworksCards from './fireworksCards'
-
+import {useState, useEffect} from 'react'
 function FireworksContainer(props) {
+
+  const [srch, setSearch] = useState("")
   console.log('Props', props.fwks)
-  const fireW = props.fwks && props.fwks.map((f) => <FireworksCards fc={f} key={f.id} onEdit={props.onEdit}/>)
+  const searchResults = props.fwks.filter(fw => {
+    return fw.name.toLowerCase().includes(srch.toLowerCase())
+  })
+  const fireW = searchResults && searchResults.map((f) => <FireworksCards fc={f} key={f.id} onEdit={props.onEdit}/>)
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+    console.log('Changed')
+  }
+
+  console.log('Search', srch)
+
   return (
     <>
-      <input type="text" className='srch' placeholder='Search'></input>
+      <input type="text" className='srch' placeholder='Search' onChange={handleChange}></input>
       <div className='container'>
         {fireW}
       </div>
