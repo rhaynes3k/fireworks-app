1d28d1fcb4ed4656e053c5d50376985946ef54b2
rhaynes3k <rhaynes3k@gmail.com>
2022-12-16 12:15:15 -0600

    Accidently deleted HomeImage component. Restored.

---
+++ src/Components/homeImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useState} from 'react'
+
+const HomeImage = ({sl}) => {
+
+  return(
+
+    <div>
+      <img  className='homePic' src='/images/genericBomb.jpeg' />
+    </div>
+  )
+}
+export default HomeImage
